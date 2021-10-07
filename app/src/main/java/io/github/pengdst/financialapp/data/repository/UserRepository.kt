1f62c288cf8ce375bfb861b490301f40dbb0157d
pengdst <pengkuh.dst@gmail.com>
2021-10-07 10:41:34 +0700

    rename typo

--- app/src/main/java/io/github/pengdst/financialapp/data/repository/UserRepository.kt
+++ app/src/main/java/io/github/pengdst/financialapp/data/repository/UserRepository.kt
@@ -2,26 +2,26 @@ package io.github.pengdst.financialapp.data.repository
 
 import io.github.pengdst.financialapp.data.remote.ApiService
 import io.github.pengdst.financialapp.data.remote.model.UserDto
-import io.github.pengdst.financialapp.data.vo.ResultReource
+import io.github.pengdst.financialapp.data.vo.ResultResource
 import io.github.pengdst.financialapp.domain.model.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class UserRepository(private val apiService: ApiService) {
+class UserRepository(private val apiService: ApiService) : BaseRepository() {
 
-    suspend fun loadUserProfile(userId: Int): ResultReource<User?> = withContext(Dispatchers.IO) {
+    suspend fun loadUserProfile(userId: Int): ResultResource<User?> = withContext(Dispatchers.IO) {
         try {
             val response = apiService.getDetailUser(userId)
             if (response.isSuccessful) {
                 val user = response.body()?.let { dtoToUser(it) }
-                ResultReource.Success(
+                ResultResource.Success(
                     data = user
                 )
             }else {
-                ResultReource.Error()
+                ResultResource.Error()
             }
         }catch (e: Exception) {
-            ResultReource.Error(message = e.localizedMessage)
+            ResultResource.Error(message = e.localizedMessage)
         }
     }
 
