9554000166823817c3646a640806a2d531260bdc
pengdst <pengkuh.dst@gmail.com>
2021-10-07 09:31:01 +0700

    refactor rename Result.kt -> ResultReource.kt

--- app/src/main/java/io/github/pengdst/financialapp/data/vo/Result.kt
+++
@@ -1,6 +0,0 @@
-package io.github.pengdst.financialapp.data.vo
-
-sealed class Result<Data> {
-    data class Success<Data>(val data: Data, val message: String = "Success Get Data."): Result<Data>()
-    data class Error<Data>(val data: Data? = null, val message: String = "Unknown Error!"): Result<Data>()
-}
